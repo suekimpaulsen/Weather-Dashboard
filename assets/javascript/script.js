4760d313efb1ecc322b3cae832128ea119c91c92
Sue Kim Paulsen <suekimpaulsen@gmail.com>
2021-05-25 20:57:21 -0500

    search button working, fetch -in progress

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -5,7 +5,21 @@ var searchHistory = JSON.parse(localStorage.getItem("search"));
 if(!searchHistory){
   var searchHistory = [];
 }else{
-  generateHistory();
+//   generateHistory();
+    console.log("else")
 }
 
 // Search Function
+function searchCity() {
+    var cityName = inputEl.val();
+    getWeather(cityName);
+    localStorage.setItem("search", JSON.stringify(searchHistory));
+    // generateHistory();
+    console.log(cityName)
+}
+searchBtn.on("click", searchCity)
+
+function getWeather(cityName) {
+    var apiURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=de7012882f4c3c7eb567c8837eb90a14";
+}
+
