39d538b8cb0cbcb8ececffbec73c066ef2fa667d
Sue Kim Paulsen <suekimpaulsen@gmail.com>
2021-05-27 21:19:04 -0500

    current date & history/clear button working

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -16,6 +16,7 @@ function searchCity() {
     getWeather(cityName);
     searchHistory.unshift(cityName);
     localStorage.setItem("search",JSON.stringify(searchHistory));
+    $(".hide").removeAttr("hidden");
     generateHistory();
 }
  
@@ -26,16 +27,16 @@ function getWeather(cityName) {
          return response.json();
      })
       .then(function(data) {
-          //append to .currentweather
           var currentTitle = document.createElement("h2")
           var weatherIcon = document.createElement("img")
           var temperature = document.createElement("p")
           var humidity = document.createElement("p")
           var windSpeed = document.createElement("p")
+          var currentDate = moment().format("l")
 
           $(currentTitle).attr("id", "city-name")
           .addClass("city-name align-middle")
-          .html(data.name)
+          .html(data.name + " (" + currentDate + ")")
 
           $(weatherIcon).addClass("weather-icon")
           .attr("src", "http://openweathermap.org/img/wn/" + data.weather[0].icon + ".png")
@@ -50,39 +51,38 @@ function getWeather(cityName) {
           $(windSpeed).attr("id", "wind-speed")
           .html("Wind speed: " + data.wind.speed + "MPH");
 
-          $(".currentWeather").html("").append(currentTitle, weatherIcon, temperature, windSpeed, humidity)
+          $("#currentWeather").addClass("white-card").html("").append(currentTitle, weatherIcon, temperature, windSpeed, humidity)
           
           callUV(data.coord.lat, data.coord.lon);
-
       })
-    
+
     var forecastURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&units=imperial&appid=de7012882f4c3c7eb567c8837eb90a14";
     fetch(forecastURL)
      .then(function(response) {
          return response.json();
      })
      .then(function(data) {
-         $(".forecast").html("")
+         $("#forecast").html("")     
          for (i = 0; i < 5; i++) {
              var card = $("<div>");
              card.addClass("card text-white bg-secondary mb-3 p-2")
-             $(".forecast").append(card)
+             $("#forecast").append(card)
 
-             var forecastTitle = document.createElement("h5")
+             var forecastDates = document.createElement("h5")
+             var futureDates = moment().add(i+1, "days").format("l")
              var weatherIcon = document.createElement("img")
              var temperature = document.createElement("p")
              var humidity = document.createElement("p")
              var windSpeed = document.createElement("p")
 
-
-            //  $(forecastTitle).text(data.name)
-             $(weatherIcon).addClass("weather-icon-small")
+             $(forecastDates).html(futureDates)
+             $(weatherIcon).addClass("icon-small")
              .attr("src", "http://openweathermap.org/img/wn/" + data.list[i].weather[0].icon + ".png")
              .attr("alt", data.list[i].weather[0].description);
              $(temperature).html("Temp: " + data.list[i].main.temp + "°F")
              $(humidity).html("Humidity: " + data.list[i].main.humidity + "%")
              $(windSpeed).html("Wind: " + data.list[i].wind.speed + "MPH")
-             card.append(card, forecastTitle, temperature, humidity, windSpeed)
+             card.append(card, forecastDates, weatherIcon, temperature, humidity, windSpeed)
 
          }
 
@@ -97,40 +97,46 @@ function callUV(lat, lon) {
         return response.json();
      })
      .then(function(data) {
-        // data.current.uvi
         var uvEl = document.createElement("p")
         $(uvEl).text("UV index: ")
-        var uvColor = document.createElement("button")
-        $(uvColor).attr("type", "button")
-        .addClass("btn btn-sm").html(data.current.uvi)
+        var uvColor = document.createElement("span")
+        $(uvColor).addClass("btn btn-sm").html(data.current.uvi)
 
         if (data.current.uvi >= 0 && data.current.uvi < 3) {
-            $(uvColor).addClass("bg-green")
+            $(uvColor).addClass("btn-success")
         }
         else if (data.current.uvi >=3 && data.current.uvi < 6) {
-            $(uvColor).addClass("bg-yellow")
+            $(uvColor).addClass("btn-yellow")
         }
         else if (data.current.uvi >= 6 && data.current.uvi < 8) {
-            $(uvColor).addClass("bg-orange")
+            $(uvColor).addClass("btn-orange")
         }
         else if (data.current.uvi >= 8 && data.current.uvi < 11) {
-            $(uvColor).addClass("bg-red")
+            $(uvColor).addClass("btn-danger")
         }
         else {
-            $(uvColor).addClass("bg-violet")
+            $(uvColor).addClass("btn-violet")
         }
-        $(".currentWeather").append(uvEl)
+        $("#currentWeather").append(uvEl)
         uvEl.append(uvColor)
      })
 
 }
 
 function generateHistory(){
-    $("ul").html("");
+    $("#history").html("");
     for(i = 0; i < searchHistory.length; i++){
       var historyEl = document.createElement("li")
-      $(historyEl).addClass("list-group-item").text(searchHistory[i]);
-      $("ul").append(historyEl)
+      $(historyEl).addClass("list-group-item btn bg-light").text(searchHistory[i]);
+      $("#history").append(historyEl)
     }
 }
 
+$("#history").on("click", "li", function() {
+    $(".hide").removeAttr("hidden");
+    getWeather($(this).text());
+})
+
+$("#clear-button").on("click", function(){
+    localStorage.clear();
+})
